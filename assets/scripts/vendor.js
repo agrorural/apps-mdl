55e9c2ebe41cf1fb67a0df7ac8b88c54538a32b2
Marcial Soto <marcialsotosanchez@gmail.com>
2018-07-23 09:49:35 -0500

    Simplebar support

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -5,6 +5,8 @@ window.jQuery = require('jquery');
 
 require('materialize-css');
 
+import SimpleBar from 'simplebar';
+
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
